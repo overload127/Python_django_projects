30b36f9a7488463e1bfb5e65f0cf257a380f8065
Kostya Vinogradov <kostya.06.05.1995@gmail.com>
2020-05-19 10:47:06 +0300

    Подготовлено все для реализации авторизации кроме вьюшки.

--- my_first_blog_board/main/urls.py
+++ my_first_blog_board/main/urls.py
@@ -6,4 +6,7 @@ from main import views
 app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('registration/', views.registration, name='registration'),
 ]
--- my_first_blog_board/main/views.py
+++ my_first_blog_board/main/views.py
@@ -1,7 +1,43 @@
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
 
 
 def index(request):
     template = 'main/index.html'
-    context = {}
+
+    if isinstance(request.user, AnonymousUser):
+        name = 'Гость'
+    elif request.user.first_name:
+        name = request.user.first_name
+    else:
+        name = request.user
+    context = {
+        'name': name,
+    }
+    return render(request, template, context)
+
+
+def login(request):
+    template = 'main/login.html'
+    if request.method == 'POST':
+        pass
+    else:
+        pass
+    context = {
+    }
+    return render(request, template, context)
+
+
+def logout(request):
+    template = 'main/logout.html'
+    context = {
+    }
+    return render(request, template, context)
+
+
+def registration(request):
+    template = 'main/registration.html'
+    context = {
+    }
     return render(request, template, context)
