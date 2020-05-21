80cf88f333aaa2cbfd32e8b9d3b2e6d742175c95
Kostya Vinogradov <kostya.06.05.1995@gmail.com>
2020-05-21 17:10:13 +0300

    Создан блог с возможностью оставлять коментарии и апрувить их автором статей. Закончено создание сайта по гайду.

--- djangogirls_site/blog/views.py
+++ djangogirls_site/blog/views.py
@@ -1,3 +1,103 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from . models import Post, Comment
+from .forms import PostForm, CommentForm
+
+
+def post_list(request):
+    all_posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    context = {'all_posts': all_posts}
+    return render(request, 'blog/post_list.html', context=context)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    context = {'post': post}
+    return render(request, 'blog/post_detail.html', context=context)
+
+
+@login_required
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            # post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    context = {'form': form}
+    return render(request, 'blog/post_edit.html', context=context)
+
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            # post.published_date = timezone.now()
+            post.pk = pk
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    context = {'form': form}
+    return render(request, 'blog/post_edit.html', context=context)
+
+
+@login_required
+def post_draft_list(request):
+    all_posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    context = {'all_posts': all_posts}
+    return render(request, 'blog/post_list.html', context=context)
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
