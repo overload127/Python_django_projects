127c05e5286d193315cc1e4a5d9be9b86d2a8fbb
Kostya Vinogradov <kostya.06.05.1995@gmail.com>
2020-05-27 16:53:57 +0300

    Добавил возможность выбирать доступные уроки из админки а так же выбирать юзеров для уроков. Работает но эфективность просядет по мере увеличения элементов (юзеров и уроков).

--- my_first_blog_board/main/admin.py
+++ my_first_blog_board/main/admin.py
@@ -6,7 +6,10 @@ from django.contrib.auth.forms import UserChangeForm
 from . import models
 
 
-@admin.register(User)
+class LessonsInlineAdmin(admin.TabularInline):
+    model = models.AdvUser.lessons.through
+
+@admin.register(models.AdvUser)
 class AdvUserAdmin(admin.ModelAdmin):
     change_user_password_template = None
     fieldsets = (
@@ -25,6 +28,7 @@ class AdvUserAdmin(admin.ModelAdmin):
     ordering = ('username',)
     filter_horizontal = ('groups', 'user_permissions',)
     readonly_fields = ('last_login', 'date_joined')
+    inlines = (LessonsInlineAdmin,)
     form = UserChangeForm
 
     def lookup_allowed(self, lookup, value):
@@ -33,4 +37,39 @@ class AdvUserAdmin(admin.ModelAdmin):
             'password') and super().lookup_allowed(lookup, value)
 
 
-admin.site.register(models.AdvUser, AdvUserAdmin)
+class FollowersInlineAdmin(admin.TabularInline):
+    model = models.Lesson.followers.through
+
+
+@admin.register(models.Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('title',)}),
+        (_('Personal info'), {'fields': ('author',)}),
+        (_('Important dates'), {'fields': ('created', 'update', 'published')}),
+    )
+    inlines = (FollowersInlineAdmin,)
+    readonly_fields = ('update',)
+    list_display = ('id', 'title', 'author', 'created', 'update', 'published')
+    search_fields = ('title', 'author')
+
+    class Meta:
+        model = models.Lesson
+
+
+@admin.register(models.LesonTube)
+class LesonTubeAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('title',)}),
+        (_('Personal info'), {'fields': ('author',)}),
+        (_('Important dates'), {'fields': ('created',)}),
+    )
+    readonly_fields = ('created',)
+    list_display = ('id', 'title', 'author', 'created')
+    search_fields = ('title', 'author')
+
+    class Meta:
+        model = models.LesonTube
+
+
+# admin.site.register(models.AdvUser, AdvUserAdmin)
--- my_first_blog_board/main/models.py
+++ my_first_blog_board/main/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class AdvUser(AbstractUser):
@@ -7,18 +8,65 @@ class AdvUser(AbstractUser):
         default=False, db_index=True, verbose_name='Прошел активацию?')
     send_message = models.BooleanField(
         default=True, verbose_name='Слать оповещения о новых коментариях?')
+    lessons = models.ManyToManyField(
+        'Lesson', through='FollowerOfLesson', related_name='+',
+        verbose_name='Доступные уроки')
 
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
 
 
+class Lesson(models.Model):
+    title = models.CharField(
+        max_length=250, verbose_name='Название урока')
+    followers = models.ManyToManyField(
+        'AdvUser', through='FollowerOfLesson', related_name='+',
+        verbose_name='Подписчики на урок')
+    author = models.ForeignKey(
+        AdvUser, blank=True, null=True, default=None,
+        on_delete=models.SET_NULL, verbose_name='Автор урока')
+    created = models.DateTimeField(
+        auto_now_add=False, auto_now=False, default=timezone.now,
+        verbose_name='Создан')
+    update = models.DateTimeField(
+        auto_now_add=False, auto_now=True, verbose_name='Изменен')
+    published = models.DateTimeField(
+        auto_now_add=False, db_index=True, blank=True, null=True,
+        verbose_name='Опубликовано')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name_plural = 'Уроки'
+        verbose_name = 'Урок'
+
+
+class FollowerOfLesson(models.Model):
+    user = models.ForeignKey(AdvUser, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} - {self.lesson}'
+
+    class Meta:
+        db_table = 'follower_of_lesson'
+
+
 class LesonTube(models.Model):
     title = models.CharField(
         max_length=250, db_index=True, verbose_name='Название видео-урока')
+    author = models.ForeignKey(
+        AdvUser, null=True, default=None, on_delete=models.SET_NULL,
+        verbose_name='Автор урока')
     path_file = models.FileField(max_length=100, verbose_name='Видео-урок')
+    created = models.DateTimeField(
+        auto_now_add=False, auto_now=False, default=timezone.now,
+        verbose_name='Создан')
 
     def __str__(self):
-        return ''
+        return f'{self.title}'
 
     class Meta:
-        pass
+        verbose_name_plural = 'Видео-уроки'
+        verbose_name = 'Видео-урок'
